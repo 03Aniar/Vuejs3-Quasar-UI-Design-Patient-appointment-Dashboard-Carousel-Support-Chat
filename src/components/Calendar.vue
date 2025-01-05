7140ed0e888d88dccfa71a57da2714482fe55e64
03Aniar <aniar.kaldybaiuly04042003@gmail.com>
2025-01-05 17:25:36 +0500

    created calendar view

---
+++ src/components/Calendar.vue
@@ -0,0 +1,16 @@
+<template>
+
+    <q-page-container>
+        <q-page class="q-pa-md">
+          <h1 class="text-h4">Welcome to My App</h1>
+          <p>This is the main body content of the app. Customize it as needed.</p>
+          <q-btn label="Click Me" color="primary" @click="onButtonClick" />
+        </q-page>
+    </q-page-container>
+</template>
+
+<script>
+export default {
+  name: 'PageCalendar'
+}
+</script>
